dff122d2edbcb17fa40234d26e6bf494a549c3fe
decipher07 <social.deej@gmail.com>
2020-05-23 16:48:38 +0530

    Added Transaction Verification Auth (Refactored Verify Trans)

--- blockchain.py
+++ blockchain.py
@@ -141,8 +141,7 @@ class Blockchain:
         if self.hosting_node == None :
             return False 
         transaction = Transaction(sender, recipient, signature, amount)
-        if not Wallet.verify_transaction(transaction):
-            return False 
+
         if Verification.verify_transaction(transaction, self.get_balance):
             self.__open_transactions.append(transaction)   
             self.save_data()
@@ -160,12 +159,15 @@ class Blockchain:
 
         reward_transaction = Transaction('MINING', self.hosting_node, '', MINING_REWARD)    
         copied_transactions = self.__open_transactions[:]
+
+        for tx in copied_transactions:
+            if not Wallet.verify_transaction(tx):
+                return False 
+
         copied_transactions.append(reward_transaction)
         block = Block(len(self.__chain), hashed_block, copied_transactions, proof )
         
-        for tx in block.transactions:
-            if not Wallet.verify_transaction(tx):
-                return False 
+      
         
         self.__chain.append(block)
 
