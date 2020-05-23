ea3aac16269d81df8d8d74f43a9b4e33549017ae
decipher07 <social.deej@gmail.com>
2020-05-23 17:54:35 +0530

    Added Route For Blockchain

--- node.py
+++ node.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 from wallet import Wallet
@@ -15,7 +15,11 @@ def get_ui():
 
 @app.route('/chain', methods=['GET'])
 def get_chain():
-    chain_snapshot = 
+    chain_snapshot = Blockchain.chain 
+    dict_chain = [block.__dict__.copy() for block in chain_snapshot]
+    for dict_block in dict_chain:
+        dict_block['transactions'] = [ tx.__dict__ for tx in dict_block['transactions']]
+    return jsonify(dict_chain), 200  
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
